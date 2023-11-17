5b675d062194dbf2c57c715ae0bf32f1a1283128
DVMaja <majadreilinger@gmail.com>
2023-11-17 12:30:28 +0100

    view elérése, attachement működik variablev-el

---
+++ app/Mail/TesztEmail.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Attachment;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Queue\SerializesModels;
+
+class TesztEmail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    /**
+     * Create a new message instance.
+     */
+    public function __construct(private string $name)
+    {
+        //
+    }
+
+    /**
+     * Get the message envelope.
+     */
+    public function envelope(): Envelope
+    {
+        return new Envelope(
+            subject: 'Teszt Email',
+        );
+    }
+
+    /**
+     * Get the message content definition.
+     */
+    public function content(): Content
+    {
+        return new Content(
+            view: 'mail.test-email',//az első a mappa neve, a második blade neve lesz
+            with: ['name' => $this->name]
+        );
+    }
+
+    
+
+    /**
+     * Get the attachments for the message.
+     *
+     * @return array<int, \Illuminate\Mail\Mailables\Attachment>
+     */
+    public function attachments(): array
+    
+    {
+        $aktualisMappa = 'proba'; //ide kell valahogy majd megérkeznie a szükséges mappának
+        $aktualisPdf = '/proba_pdf.pdf';
+        return [
+            Attachment::fromPath('storage/'.$aktualisMappa.$aktualisPdf)
+                    ->as('kuldendo.pdf')
+                    ->withMime('application/pdf'),
+        ];
+    }
+}
