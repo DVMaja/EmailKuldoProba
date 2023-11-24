62a8fd95f72182bd093111106966e3995af32a2f
DVMaja <majadreilinger@gmail.com>
2023-11-24 11:07:12 +0100

    formázott üzenet

--- resources/views/mail/test-email.blade.php
+++ resources/views/mail/test-email.blade.php
@@ -1,4 +1,6 @@
-Hey {{$name}},
-Ez egy teszt email, SMPT segítsével.
-Tszt variables-ekkel
-Laravel
+<h2>Hey, {{$name}},</h2> 
+<p>Ez egy teszt email, SMPT segítsével.</p>
+<br>
+<p>Mostmár formázva</p>
+<br>
+<h3>Automatizált üzenethez</h3>
