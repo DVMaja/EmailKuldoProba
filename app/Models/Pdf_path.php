03eb554b53829b75a19759807d62641ef11227b5
DVMaja <majadreilinger@gmail.com>
2023-12-01 15:20:13 +0100

    Pdf_path kész

---
+++ app/Models/Pdf_path.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Pdf_path extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'student_id',
+        'path',
+        'major_id',
+        'year',
+        'month',
+    ];
+}
---
+++ database/migrations/2023_12_01_084159_create_pdf_paths_table.php
@@ -0,0 +1,47 @@
+<?php
+
+use App\Models\Pdf_path;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('pdf_paths', function (Blueprint $table) {
+            //$table->id();
+            $table->foreignId('student_id')->references('student_id')->on('majors');
+            $table->string('path');
+            $table->primary(['year', 'month', 'path']);
+            $table->year('year');
+            $table->unsignedTinyInteger('month');
+            $table->timestamps();
+        });
+
+        Pdf_path::create([
+            'student_id' => '00524',
+            'path' => '202301',
+            'year' => 2023,
+            'month' => 01,
+        ]);
+
+        Pdf_path::create([
+            'student_id' => '00525',
+            'path' => '202301',
+            'year' => 2023,
+            'month' => 01,
+        ]);
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('pdf_paths');
+    }
+};
